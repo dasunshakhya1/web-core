de2f10b808d3215bbca091af10d2f8d00d2202f8
dasun <dasun@wso2.com>
2022-12-29 17:54:08 +0530

    adding new tests methods

--- src/main/java/core/Configs.java
+++ src/main/java/core/Configs.java
@@ -1,11 +1,8 @@
 package core;
 
-import java.net.URI;
-import java.util.Objects;
-
 public class Configs {
 
     public static final String BROWSER = System.getenv("browser")!=null?System.getenv("browser"):"CHROME";
-    public static final String BASE_URL = "https://www.saucedemo.com/";
+    public static final String BASE_URL = System.getenv("baseUrl")!=null?System.getenv("baseUrl"):"https://www.saucedemo.com/";
 
 }
